ba37e72d0ac7878a6dacc4da618797bffdf2dd57
Tara E. Johnson <74938013+taraejohnson@users.noreply.github.com>
2021-02-19 12:33:19 -0500

    Delete frontend directory

--- frontend/js/header.js
+++
@@ -1,8 +0,0 @@
-const createHeader = function () {
-    const header = document.createElement("header");
-    header.classList.add("main-header");
-    header.innerHTML = `<h1>Stanza</h1>`;
-    return header;
-  };
-  
-  export { createHeader };
